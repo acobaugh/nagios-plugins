433863496b0a063e9b278e41b0e646839b60fa2c
Andy Cobaugh <atc135@psu.edu>
2012-05-03 14:52:26 -0400

    also check failed count

--- ldap/check_ibmldap_replication.pl
+++ ldap/check_ibmldap_replication.pl
@@ -7,7 +7,8 @@ use Nagios::Plugin;
 use Net::LDAP;
 use Getopt::Long;
 
-my $pending_attr = 'ibm-replicationpendingchangecount';
+my $pending_attr = 'ibm-replicationPendingChangeCount';
+my $failed_attr = 'ibm-replicationFailedChangeCount';
 
 sub usage {
 	print "Usage: $0 -H <LDAP SLAVE> -m <LDAP MASTER> -b <BASE> [-g <REPLICATION GROUP>] [-w <WARNING COUNT>] [-c <CRITICAL COUNT>] [-h] [-v]\n";
@@ -42,53 +43,68 @@ if (!$o{'b'} or !$o{'H'} or !$o{'m'}) {
 	usage();
 }
 
-if ($o{'c'} lt $o{'w'} or $o{'c'} eq $o{'w'}) {
+# sanity-check -w and -c
+if ($o{'c'} <= $o{'w'}) {
 	print "WARNING (-w) must be less than CRITICAL (-c)\n";
 	usage();
 }
 
+# build search string
 my $search = sprintf("cn=%s:636,cn=%s:636,ibm-replicaGroup=%s,%s", $o{'H'}, $o{'m'}, $o{'g'}, $o{'b'});
 if ($o{'v'}) {
 	printf "DEBUG: search = %s\n", $search;
 }
 
+# connect
 my $ldap;
 if (! ($ldap = Net::LDAP->new($o{'m'})) ) {
 	printf "UNKNOWN: Failed to create ldap object for %s: %s\n", $o{'m'}, $@;
 	exit UNKNOWN;
 }
 
+# bind
 my $mesg = $ldap->bind();
 if ($mesg->code) {
 	printf "UNKNOWN: Failed to bind to %s: %s\n", $o{'m'}, $mesg->error;
 	exit UNKNOWN;
 }
 
+# search
 $mesg = $ldap->search(
 	base => $search,
 	scope => 'base',
 	filter => '(objectClass=ibm-replicationagreement)',
-	attrs => [ $pending_attr ],
+	attrs => [ $pending_attr, $failed_attr ],
 	sizelimit => 1
 );
 
+# count results
 if ($mesg->count ne 1) {
 	printf "CRITICAL: LDAP search returned %i entries\n", $mesg->count;
 	exit CRITICAL;
 }
 
+# get attr values
 my @entries = $mesg->entries;
 my $pending = $entries[0]->get_value($pending_attr);
+my $failed = $entries[0]->get_value($failed_attr);
 
+my $statusline = sprintf("%s = %i, %s = %i", $pending_attr, $pending, $failed_attr, $failed);
 
-if ($pending le $o{'w'}) {
-	printf "OK: %s = %i\n", $pending_attr, $pending;
+# failed changes take precedence
+if ($failed > 0) {
+	printf "CRITICAL: %s\n", $statusline;
+	exit CRITICAL;
+}
+
+if ($pending <= $o{'w'}) {
+	printf "OK: %s\n", $statusline;
 	exit OK;
-} elsif ($pending le $o{'c'}) {
-	printf "WARNING: %s = %i\n", $pending_attr, $pending;
+} elsif ($pending <= $o{'c'}) {
+	printf "WARNING: %s\n", $statusline;
 	exit WARNING;
 } else {
-	printf "CRITICAL: %s = %i\n", $pending_attr, $pending;
+	printf "CRITICAL: %s\n", $statusline;
 	exit CRITICAL;
 }
 
